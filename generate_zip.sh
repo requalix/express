69f39c2fa8a360c885514fc6bf49727423d5da43
kitten <jack@murray.cx>
2013-04-09 23:19:32 +1000

    gen zip

--- generate_zip.sh
+++ generate_zip.sh
@@ -2,9 +2,9 @@
 echo make sure the server is running on port 3000
 
 curl localhost:3000 |
-  sed -e 's/\/music/public\/music/g' \
-      -e 's/\/javascripts/\/public\/javascripts/g' \
-      -e 's/\/stylesheetss/\/public\/stylesheetss/g' > index.html
+  sed -e 's/\/music/.\/public\/music/g' \
+      -e 's/\/javascripts/.\/public\/javascripts/g' \
+      -e 's/\/stylesheetss/.\/public\/stylesheetss/g' > index.html
 
 rm express.zip
 ln -s . express
